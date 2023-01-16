6b7c1d2df98e5cd713db1722b52a7f06600606f0
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2023-01-16 20:22:41 -0300

    O que é Banco de Dados

---
+++ aula2.js
@@ -0,0 +1,8 @@
+// Um banco de dados é uma coleção de dados logicamente relacionados, com algum significado. Implica em que associações aleatórias de dados não possam ser chamadas de bases de dados. É projetado, construido e preenchido com dados para um proposito especifico. possui um grupo de usuarios e algumas aplicações concebidas para atender a esses usuarios.Implicaé a representação abstrata de uma realidade em um sistema eletronico.Implicacom esta representacao, podemos gerar instancias, ou seja, dados concretos provenientes de uma realidade.
+
+
+// o que é banco de dados. 
+
+// um banco de dados é uma coleção organizada de informações - ou dados - estruturadas, normalmente armazenadas eletronicamente em um sistema de computador, que permite a consulta e a atualização de dados pelo usuário.a
+
+// dado - é o valor do campo quando é armazenado no banco de dados
