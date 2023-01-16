95962ab30f72bea233c8a5c5a61dd3eb98181667
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2023-01-16 19:50:01 -0300

    Introdução ao Banco de Dados

---
+++ aula1.js
@@ -0,0 +1,14 @@
+
+// notas mentais
+
+// > Persistir (guardar e manter)
+
+// > Planejar antes de começar a escrever código
+
+// > DBA (administrador de banco de dados mantem a gerencia e "saúde" do BD)
+
+// > Programador tem que ter entendimento de como acessar e manipular o BD
+
+// > Projetista vai estudar as regras de negócio para o desenvolvedor
+
+// > O BD deve ser capaz de manter e organizar os dados necessários
